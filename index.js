62b5d617c16f9e84c92dcdfc308f1bf18e057595
Daniil Zaytsev <danyazaytsev@gmail.com>
2021-12-25 16:04:00 +0300

    Add inline buttons and answer callback query

--- index.js
+++ index.js
@@ -20,15 +20,36 @@ bot.onText(/\/get/, (msg, match) => {
     pool.connect();
     const delay = () => new Promise(resolve => setTimeout(resolve, 500));
 
-    pool.query('SELECT * FROM public.posts WHERE is_posted IS false ORDER BY id DESC', async (err, res) => {
+    const options = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{text: '✅', callback_data: '1'}, {text: '❌', callback_data: '2'}],
+            ]
+        }),
+        parse_mode: 'HTML'
+    };
+    pool.query('SELECT * FROM public.posts ORDER BY id DESC', async (err, res) => {
         const posts = res['rows'];
         for (let i = 0; i < posts.length; i++) {
             console.log(posts[i]['title']);
             let message = `<b>${posts[i]['title']}</b>\n\n${posts[i]['link']}`
-            bot.sendMessage(chatId, message, {'parse_mode':'HTML'});
+            await bot.sendMessage(chatId, message, options);
             await delay();
-
         }
         pool.end();
     });
+
+});
+
+bot.on('callback_query', (ev) => {
+    const chatId = ev.message.chat.id
+
+    if (ev.data === '1') {
+        bot.answerCallbackQuery(ev.id, 'Liked').then(() => {
+            bot.sendMessage(chatId, 'Лайк принят');
+        });
+    } else
+        bot.answerCallbackQuery(ev.id, 'Disliked').then(() => {
+            bot.sendMessage(chatId, 'Дизлайк принят');
+        });
 });
